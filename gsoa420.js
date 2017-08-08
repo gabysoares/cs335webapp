0f6f006c6ca14555d34db298ee6e384025eb2c02
YOUR NAME <gabriella.soares03@gmail.com>
2017-08-08 12:36:20 +1200

    everything working except register and refreshing comments

--- gsoa420.js
+++ gsoa420.js
@@ -8,108 +8,32 @@ function home()
   document.getElementById("displayText").innerHTML = str;
 }
 
-function myFunction()
-{
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav")
-  {
-    x.className += " responsive";
-  }
-  else
-  {
-    x.className = "topnav";
-  }
-}
 
-// function comments()
-// {
-//   var str = "<h1 style='color:white;'>Guest Book</h1> <p style = 'color:white;'>Please sign our guest book. Your comments are greatly appreciated</p><div><table style= 'background-color: #323232; padding: 15px; border-radius:25px; width:100%;' align='center'> <tr><td colspan='3'><textarea id='text' rows='6' cols='50' style= 'width:100%'></textarea></td></tr><tr><td style= 'text-align: center;'> Your Name:</td><td style= 'text-align: center;'> <input id='name' type='text' name='name' placeholder='Enter your name here'></td><td style= 'text-align: center;' onclick='showcomments()'>button</td></tr></table></div>";
-//   document.getElementById("menu").innerHTML = str;
-//   var yourname = document.getElementById("name").value;
-//   var yourtext = document.getElementById("text").value;
-//   var uri = "http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/comment?name={NAME}";
-//   var xhr = new XMLHttpRequest();
-//   xhr.open("POST", uri, true);
-//   xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-//   xhr.onload = function() {}
-//   xhr.send(JSON.stringify(objectToPost));
-
-// }
-  // function getComments()
-  // {
-  //   var uri = "http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/htmlcomments";
-  //   var xhr = new XMLHttpRequest();
-  //   xhr.open("GET", uri, true);
-  //   xhr.setRequestHeader("Accept", "application/html");
-  //   xhr.onload = function()
-  //   {
-  //     var response = xhr.responseText;
-  //     showComments(response);
-  //   }
-  //   xhr.send(null);
-  // }
-
-// function getComments(){
-//     var uri = "http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/htmlcomments";
-//     var xhr = new XMLHttpRequest();
-//     xhr.open("GET", uri, true);
-//     xhr.setRequestHeader("Accept", "application/json");
-//     xhr.onload = function()
-//     {
-//       var response = JSON.parse(xhr.responseText);
-//       showComments(response);
-//     }
-//     xhr.send(null);
-//  }
-
-function showComments(comments){
-	var content = "<p>";
-	for (var i = 0; i <comments.length; i++)
-	{
-		var comment = comments[i];
-		content += comment + "<br/>"
-	}
-	content += "</p>"
-
-	document.getElementById("displayText").innerHTML = content;
-}
 
 // function showComments(){
-// 	var commentBox = "<form action=&#34;/postComment();>First name:<br><input type=&#34;text&#34; name=&#34;Comment&#34; value=&#34;Mickey&#34;><br>Last name:<br><input type=&#34;text&#34; name=&#34;lastname&#34; value=&#34;Mouse&#34;><br><br><input type=&#34;submit&#34; value=&#34;Submit&#34;></form> "
-//      document.getElementById("displayText").innerHTML='<object type="text/html" data="http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/htmlcomments" ></object>';
+// 	//var commentBox = "<form action=&#34;/postComment();>First name:<br><input type=&#34;text&#34; name=&#34;Comment&#34; value=&#34;Mickey&#34;><br>Last name:<br><input type=&#34;text&#34; name=&#34;lastname&#34; value=&#34;Mouse&#34;><br><br><input type=&#34;submit&#34; value=&#34;Submit&#34;></form> "
+//      document.getElementById("commentArea").innerHTML='<object type="text/html" data="http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/htmlcomments" ></object>';
 // }
 
-//works
 
-function comment()
-{
-	debugger;
-  var xhr = new XMLHttpRequest();
-  var uri = "http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/comment?name=" + "fran";
-  xhr.open("POST", uri, true);
-  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  xhr.onload = function(){
-  	var comment = document.getElementById("displayText");
-  	comment.innerHTML= xhr.responseText;
-  }
-  xhr.send('"comment works"')
-}
 
 function readComment()
-{	
-
+{		
 		showSearchBar(false);
 
 		var str = "<p> <input name=\"nameEntry\" type=\"text\" value=\"Enter name here\" /></p> <p> <input name=\"commentEntry\" type=\"text\" value=\"Enter comment here\" /></p> <p> <input name=\"submitButton\" type=\"button\" onclick=\"postComment()\" value=\"Submit\" /></p>";  
-		document.getElementById("displayText").innerHTML = str;
+		
+		var comments = "<div><iframe src=\"http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/htmlcomments\" height=\"60%\" width=\"100%\"></iframe></div>";
+		//document.getElementById("commentArea").innerHTML = comments;
+		var page = str + comments;
+		document.getElementById("displayText").innerHTML = page;
 
 
 }
 
 function postComment()
 {
-	//
-	//debugger;
+
 
 	var xhr = new XMLHttpRequest();
 	var name = document.getElementsByName("nameEntry")[0].value;
@@ -117,15 +41,10 @@ function postComment()
 
 	
 	var uri = "http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/comment?name=" + name;
-	//debugger;
 	xhr.open("POST",uri,true);
 	xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-	// xhr.onload = function () {
-	// 	// var comment = document.getElementById("displayText");
- //  // 	comment.innerHTML= xhr.responseText;
-	// }
 	xhr.send(JSON.stringify(comment));
-	//document.getElementById("displayText").innerHTML = str;
+	
 }
 
 
@@ -182,7 +101,6 @@ function postComment()
   	
 
     var tableContent = "<table>";
-    //var search = 
     for (var i = 0; i < books.length; ++i)
     {
       var record = books[i];
